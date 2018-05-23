5dbbe3dab676a3c67a676cf887089b03389e79ac
Damian Conde <dconde@distecna.com>
2018-05-23 12:59:54 -0300

    Delete Unused Import

--- src/test/java/almundo/com/backend/DispatcherExtraTest.java
+++ src/test/java/almundo/com/backend/DispatcherExtraTest.java
@@ -12,7 +12,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ConfigurableApplicationContext;
